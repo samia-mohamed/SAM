c047cd2e813f2a6251328f2836d8d584e0fb8201
Samia Mohamed Abd Elaziz Sayed <msamia177@gmail.com>
2022-11-24 11:05:27 +0200

    Add files via upload

---
+++ if,nestedif.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int = 500
+
+	if a == 100 {
+
+		fmt.Printf("Value of a is 100\n")
+	} else if a == 200 {
+
+		fmt.Printf("Value of a is 200\n")
+	} else if a == 300 {
+
+		fmt.Printf("Value of a is 300\n")
+	} else {
+
+		fmt.Printf("None of the values is matching\n")
+	}
+}
