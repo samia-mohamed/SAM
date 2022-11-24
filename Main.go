a45e3375bf12c70e3f98d653ffc1a4211a09f8dc
Samia Mohamed Abd Elaziz Sayed <msamia177@gmail.com>
2022-11-24 10:32:29 +0200

    Add files via upload

---
+++ Main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var my_age = 21
+	var my_name = "samia mohamed"
+	var my_GBA = 3.4
+
+	fmt.Printf("my age is : %d\n", my_age)
+
+	fmt.Printf("The type of 'my_age' is : %T\n", my_age)
+
+	fmt.Printf("my name is : %s\n", my_name)
+
+	fmt.Printf("The type of 'my_name' is : %T\n", my_name)
+
+	fmt.Printf("my GBA is : %f\n", my_GBA)
+
+	fmt.Printf("The type of 'my_GBA' is : %T\n", my_GBA)
+}
